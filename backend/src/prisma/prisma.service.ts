724434022b835a9af49c22f2e77e7a76ddeb459a
alex8023 <alex8023@yandex.ru>
2026-05-25 15:19:24 +0300

    feat(backend): add prisma orm

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  readonly client: PrismaClient;
+
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) {
+      throw new Error('DATABASE_URL environment variable is not set');
+    }
+    const adapter = new PrismaMariaDb(connectionString);
+    this.client = new PrismaClient({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.client.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.client.$disconnect();
+  }
+}
