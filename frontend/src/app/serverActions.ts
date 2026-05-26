1badc1ef59ffd7c1191e9b201f8020710b6af7c5
alex8023 <alex8023@yandex.ru>
2026-05-26 17:27:08 +0300

    ci: add Dockerfiles & compose

--- frontend/src/app/serverActions.ts
+++ frontend/src/app/serverActions.ts
@@ -3,8 +3,10 @@
 import { revalidatePath } from 'next/cache';
 import { WorkRecordsResponseSchema, WorkTypesResponseSchema } from '@/lib/schemas';
 
+const BACKEND_URL = process.env.BACKEND_URL; // ?? 'http://localhost:3001';
+
 export async function getWorkRecords(sort: 'asc' | 'desc' = 'desc') {
-  return fetch(`http://localhost:3001/get-work-records?sort=${sort}`)
+  return fetch(`${BACKEND_URL}/get-work-records?sort=${sort}`)
     .then((res) => res.json())
     .then((data) => WorkRecordsResponseSchema.parse(data))
     .catch((err) => console.error(err));
@@ -16,7 +18,7 @@ export async function addWorkRecord(data: {
   workTypeId: string;
   volume: string;
 }) {
-  const res = await fetch('http://localhost:3001/add-work-record', {
+  const res = await fetch(`${BACKEND_URL}/add-work-record`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
@@ -26,14 +28,14 @@ export async function addWorkRecord(data: {
 }
 
 export async function getWorkTypes() {
-  return fetch('http://localhost:3001/get-work-types')
+  return fetch(`${BACKEND_URL}/get-work-types`)
     .then((res) => res.json())
     .then((data) => WorkTypesResponseSchema.parse(data))
     .catch((err) => console.error(err));
 }
 
 export async function deleteWorkRecord(id: string) {
-  const res = await fetch(`http://localhost:3001/delete-work-record?id=${id}`, {
+  const res = await fetch(`${BACKEND_URL}/delete-work-record?id=${id}`, {
     method: 'DELETE',
   });
   if (!res.ok) throw new Error('Failed to delete work record');
